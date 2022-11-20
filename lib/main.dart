af4a13203509ef561048252d79c3210ad12a7eaa
Abbas Endari <a.endari@gmail.com>
2022-11-20 20:54:42 +0330

    material3 theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,11 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blueGrey,
+        useMaterial3: true,
+      ),
       home: Scaffold(
-        backgroundColor: Colors.red,
-        body: SafeArea(
-          child: Container(),
-        ),
+        body: SafeArea(child: ,),
       ),
     );
   }
